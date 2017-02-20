caff5ed600abd85f4427b62f77906be9d955e5ba
Devesh vyas <Devesh vyas>
2017-02-20 11:28:41 +0530

    Initial Commit

---
+++ Assets/CurvedUI/Demo Scenes/Demo Scenes Content/CUI_ViveButtonState.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+namespace CurvedUI {
+
+    /// <summary>
+    /// This script show you how to access the state of any button on Vive Controller via CurvedUI scripts. We use right controller as an example
+    /// </summary>
+    public class CUI_ViveButtonState : MonoBehaviour
+    {
+
+        enum ViveButton
+        {
+            Trigger,
+            TouchpadTouch,
+            TouchpadPress,
+            Grip,
+            Menu,
+
+        }
+
+#pragma warning disable 414 // this is just so we wont get "unused variable" code warnings when compiling without Vive.
+        [SerializeField]
+        Color ActiveColor = Color.green;
+        [SerializeField]
+        Color InActiveColor = Color.gray;
+        [SerializeField] ViveButton ShowStateFor = ViveButton.Trigger;
+#pragma warning restore 414
+
+
+#if CURVEDUI_VIVE
+        // Update is called once per frame
+        void Update()
+        {
+           
+            if(CurvedUIViveInputModule.Right == null)
+            {
+                Debug.LogError("Right controller not found - it may be off");
+                return;
+            }
+
+            bool pressed = false;
+
+            switch (ShowStateFor)
+            {
+                case ViveButton.Trigger:
+                {
+                    pressed = CurvedUIViveInputModule.Right.IsTriggerPressed;
+                    break;
+                }
+                case ViveButton.TouchpadPress:
+                {
+                    pressed = CurvedUIViveInputModule.Right.IsTouchpadPressed;
+                    break;
+                }
+                case ViveButton.TouchpadTouch:
+                {
+                    pressed = CurvedUIViveInputModule.Right.IsTouchpadTouched;
+                    break;
+                }
+                case ViveButton.Grip:
+                {
+                    pressed = CurvedUIViveInputModule.Right.IsGripPressed;
+                    break;
+                }
+                case ViveButton.Menu:
+                {
+                    pressed = CurvedUIViveInputModule.Right.IsApplicationMenuPressed;
+                    break;
+                }
+            }
+
+            this.GetComponentInChildren<Image>().color = pressed ? ActiveColor : InActiveColor;
+        }
+#endif
+    }
+}
+
+
---
+++ Assets/CurvedUI/Demo Scenes/Demo Scenes Content/CUI_ViveLaserBeam.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CurvedUI
+{
+    /// <summary>
+    /// This class contains code that controls the mockup vive controller. 
+    /// Its made to make demo sceen look better. Its not made to be used with actual vive controller.
+    /// </summary>
+    public class CUI_ViveLaserBeam : MonoBehaviour
+    {
+
+        [SerializeField]
+        Transform LaserBeamTransform;
+        [SerializeField]
+        Transform LaserBeamDot;
+
+        
+
+        // Update is called once per frame
+        protected void Update()
+        {
+
+            //get direction of the controller
+            Ray myRay = new Ray(this.transform.position, this.transform.forward);
+
+
+            //make laser beam hit stuff it points at.
+            if(LaserBeamTransform && LaserBeamDot) {
+                //change the laser's length depending on where it hits
+                float length = 10000;
+
+                RaycastHit hit;
+                if (Physics.Raycast(myRay, out hit, length))
+                {
+                    length = Vector3.Distance(hit.point, this.transform.position);
+
+                    //If we hit a canvas, we only want transforms with graphics to block the pointer. (that are drawn by canvas => depth not -1)
+                    if (hit.transform.GetComponent<CurvedUIRaycaster>() != null)  {
+                        int SelectablesUnderPointer = hit.transform.GetComponent<CurvedUIRaycaster>().GetObjectsUnderPointer().FindAll(x => x.GetComponent<Graphic>() != null && x.GetComponent<Graphic>().depth != -1).Count;
+
+                        //Debug.Log("found graphics: " + SelectablesUnderPointer);
+                        length = SelectablesUnderPointer == 0 ? 10000 : Vector3.Distance(hit.point, this.transform.position);
+                    }  
+
+                }
+
+                LaserBeamTransform.localScale = LaserBeamTransform.localScale.ModifyZ(length);
+            }
+           
+
+        }
+    }
+}
